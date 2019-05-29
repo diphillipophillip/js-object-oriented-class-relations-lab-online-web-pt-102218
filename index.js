52486f98fb9bf7d078e22febca49f3e03a52e538
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-29 15:04:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,16 @@ class Trip {
     this.passengerId = passengerId
     store.trips.push(this)
   }
-  
+    driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    });
+  }
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
+  }
 }
 
 
