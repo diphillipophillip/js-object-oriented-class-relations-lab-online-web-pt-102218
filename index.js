979076730a9ee130c4d835182b9555a4aa553fd0
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-29 14:59:24 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,36 @@
+let store = {drivers: [], passengers: [], trips: []}
+let driverId = 0
+let passengerId = 0
+let tripId = 0
+
+class Driver {
+  constructor(name) {
+    this.id = ++driverId
+    this.name = name
+    store.drivers.push(this)
+  }
+}
+
+class Passenger {
+  constructor(name) {
+    this.id = ++passengerId
+    this.name = name
+    store.passengers.push(this)
+  }
+}
+
+class Trip {
+  constructor() {
+    this.id = ++tripId
+    if (driver) {
+      this.driverId = driverId
+    }
+    store.trips.push(this)
+  }
+  setDriver(driver) {
+    this.driverId = driverId
+  }
+  
+}
+
+
